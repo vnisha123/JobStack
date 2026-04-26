7ef16ec3ed7608199210aca14bd8045e155a2733
Nisha <nishavahutre001@gmail.com>
2026-04-26 18:30:54 +0530

    create create_job endroute to test pydantic model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-
+from app.schemas import Job
 app = FastAPI()
 
 @app.get("/")
 def home():
     return{"message":"this is a job application tracker"}
+
+@app.post("/add_job")
+async def create_job(job : Job):
+    return{"Job":job}
