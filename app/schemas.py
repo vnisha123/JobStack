65f6e87faeb00a3f4c48c011d0fad982aa054ea8
Nisha <nishavahutre001@gmail.com>
2026-04-26 18:25:58 +0530

    added pydantic model Job for data validation

---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field, ConfigDict
+from enum import Enum
+
+class JobStatus(str,Enum):
+    INTERESTED = "Interested"
+    APPLIED = "Applied"
+    INTERVIEWING = "Interviewing"
+    OFFER = "Offer"
+    REJECTED ="Rejected"
+
+class Job(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    company :str = Field(...)
+    role : str = Field(...)
+    status : JobStatus = JobStatus.INTERESTED
+
