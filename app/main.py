c2a2bdd84133f58369de71d77cf7bade08ec1b3d
Nisha <nishavahutre001@gmail.com>
2026-05-01 00:53:27 +0530

    added title to swagger ui

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.routers import auth, users, jobs
 
-app = FastAPI()
+app = FastAPI(title="Job Tracker API",
+    description="A backend for managing job applications with JWT auth",
+    version="1.0.0")
 
 app.include_router(auth.router)
 app.include_router(users.router)
